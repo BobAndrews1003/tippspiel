e5640d8d7eaca58a566c15753bbb9fd0a46ff2d2
Pascal <pascaldierkes@gmail.com>
2026-02-20 19:36:17 -0500

    Fix static URL + nav marker + gitignore

--- tippspiel/settings.py
+++ tippspiel/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 # Static files
 # ---------------------------------------------------------------------
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]     # <— dein Quell-Ordner
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # WhiteNoise storage (recommended)
 STORAGES = {
